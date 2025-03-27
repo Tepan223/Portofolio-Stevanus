98eb1c3b79ca16e5b0a4d743943bfc6a29fc0e50
Tepan223 <stevanusgabrielbb34@gmail.com>
2025-03-27 13:52:18 +0700

    gsap

--- src/Components/Headers.jsx
+++ src/Components/Headers.jsx
@@ -1,46 +1,19 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import gsap from "gsap";
-import "../Style/Headers.css";
+import '../Style/Headers.css'
 
 function Headers() {
-    const headerRef = useRef(null);
-    const navLinksRef = useRef([]);
-
-    useEffect(() => {
-        gsap.from(headerRef.current, {
-            y: -50,
-            opacity: 0,
-            duration: 1,
-            ease: "power2.out"
-        });
-
-        gsap.from(navLinksRef.current, {
-            y: 20,
-            opacity: 0,
-            duration: 0.8,
-            ease: "power2.out",
-            stagger: 0.2
-        });
-    }, []);
-
+    
     return (
-        <div className="header" ref={headerRef}>
+        <div className="header">
             <div className="name-head">
                 <p className="name-head-p">Stevanus Gabriel.</p>
             </div>
             <div className="link">
-                {["/", "/about&skills", "/project", "/contact"].map((path, index) => (
-                    <nav key={index}>
-                        <Link 
-                            to={path} 
-                            className="navLink" 
-                            ref={el => (navLinksRef.current[index] = el)}
-                        >
-                            {path.replace("/", "").replace("&", " & ") || "Home"}
-                        </Link>
-                    </nav>
-                ))}
+                <nav><Link to="/" className="navLink">Home</Link></nav>
+                <nav><Link to="/about&skills "className="navLink">About & Skills</Link></nav>
+                <nav><Link to="/project" className="navLink">Project</Link></nav>
+                <nav><Link to="/Contact" className="navLink">Contact</Link></nav>
             </div>
         </div>
     );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,46 +1,27 @@
-import React, { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
+import React from 'react';
 import Headers from './Headers';
-import '../Style/Home.css';
+import '../Style/Home.css'
 
 function Home() {
-  const textRef = useRef(null);
-  const imgRef = useRef(null);
-
-  useEffect(() => {
-    gsap.from(textRef.current, {
-      x: -100,
-      opacity: 0,
-      duration: 1.2,
-      ease: "power3.out"
-    });
-
-    gsap.from(imgRef.current, {
-      x: 100,
-      opacity: 0,
-      duration: 1.2,
-      ease: "power3.out"
-    });
-  }, []);
-
   return (
     <div>
-      <Headers />
+      <div>
+        <Headers/>
+      </div>
       <div className='home'>
         <div className='home-content'>
           <div className='content'>
-            <div className='text-content-home' ref={textRef}>
+            <div className='text-content-home'>
               <h1>Hi, I'm <br/><span>Stevanus Gabriel</span></h1>
               <p>Web Developer</p>
-              <p>
-                As a Web Developer, I have a great interest in creating a functional website. 
-                With expertise in website development, I always try to create websites with 
-                the most attractive designs possible.
-              </p>
+              <p>As a Web Developer, I have a great interest in creating a functional website. With expertise in website development, I always try to create websites with the most attractive designs possible.</p>
               <a href="" download className='download'>Download CV</a>
+              <div contact>
+                
+              </div>
             </div>
-            <div className='img-content-home' ref={imgRef}>
-              <img src="/Profile.jpg" alt="Profile" />
+            <div className='img-content-home'>
+              <img src="/Profile.jpg" alt="" />
             </div>
           </div>
         </div>
