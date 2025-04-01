8956b95ccde6aeb947c549f1c47f10acb29e77ec
Tepan223 <stevanusgabrielbb34@gmail.com>
2025-04-01 18:32:34 +0700

    gsap

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -6,7 +6,59 @@ function Project() {
       <div>
         <Headers/>
       </div>
-      <div className='a'></div>
+      <div className='project'>
+        <div className='project-content'>
+          <div className='content-project'>
+            <div>
+              <h1>Project</h1>
+              <div className='edit-line'>
+                <div className='line-project'>
+                  <div className='project 1'>
+                    <img src="" alt="" />
+                    <h2>Project 1</h2>
+                  </div>
+                  <div className='project 2'>
+                    <img src="" alt="" />
+                    <h2>Project 2</h2>
+                  </div>
+                  <div className='project 3'>
+                    <img src="" alt="" />
+                    <h2>Project 3</h2>
+                  </div>
+                </div>
+                <div className='line-project-2'>
+                  <div className='project 4'>
+                    <img src="" alt="" />
+                    <h2>Project 4</h2>
+                  </div>
+                  <div className='project 5'>
+                    <img src="" alt="" />
+                    <h2>Project 5</h2>
+                  </div>
+                  <div className='project 6'>
+                    <img src="" alt="" />
+                    <h2>Project 6</h2>
+                  </div>
+                </div>
+                <div className='line-project-3'>
+                  <div className='project 7'>
+                    <img src="" alt="" />
+                    <h2>Project 7</h2>
+                  </div>
+                  <div className='project 8'>
+                    <img src="" alt="" />
+                    <h2>Project 8</h2>
+                  </div>
+                  <div className='project 9'>
+                    <img src="" alt="" />
+                    <h2>Project 9</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
